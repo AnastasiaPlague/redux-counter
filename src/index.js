cbe559c8ddf91182e166089c486099f0e18e858f
Anastasia <great-swindler@mail.ru>
2020-09-24 12:57:53 +0300

    Connect Counter.js to Redux store to update counter value

--- src/index.js
+++ src/index.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 
 import './styles.scss';
 
+const initialState = {
+  count: 0,
+};
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+const increment = () => ({
+  type: INCREMENT,
+});
+
+const decrement = () => ({
+  type: DECREMENT,
+});
+
+const reset = () => ({
+  type: RESET,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        count: state.count - 1,
+      };
+    case RESET:
+      return {
+        count: 0,
+      };
+
+    default:
+      break;
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
 class Counter extends Component {
   render() {
+    const { count, increment, decrement, reset } = this.props;
+
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
         </section>
       </main>
     );
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = (state) => {
+  return state;
+};
+const mapDispatchToProps = { increment, decrement, reset };
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
